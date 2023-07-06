25fccb6c0b70e4582d22be5b1d82bbec815c1221
vivek <ikumarvivek786@gmail.com>
2023-07-06 19:48:19 +0530

    [Vivek]Access Modifiers]

---
+++ Day3/AccessModifiers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class AccessModifiers
+    {
+        //public class
+        public class Person
+        {
+            public string Name;
+            public int Age;
+
+            public void SayHello()
+            {
+                Console.WriteLine("Hello, my name is " + Name);
+            }
+        }
+
+        //private class
+        public class BankAccount
+        {
+            private decimal balance;
+
+            public void Deposit(decimal amount)
+            {
+                balance += amount;
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                if (balance >= amount)
+                {
+                    balance -= amount;
+                }
+            }
+        }
+    }
+}
---
+++ Day3/interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Interface
+    {
+
+        public interface IAnimal
+        {
+            void MakeSound();
+        }
+
+        // Implement the interface in a class
+        public class Dog : IAnimal
+        {
+            public void MakeSound()
+            {
+                Console.WriteLine("Woof!");
+            }
+        }
+
+        // Implement the interface in another class
+        public class Cat : IAnimal
+        {
+            public void MakeSound()
+            {
+                Console.WriteLine("Meow!");
+            }
+        }
+
+        // Use the implemented interface
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                IAnimal dog = new Dog();
+                IAnimal cat = new Cat();
+
+                dog.MakeSound(); // Output: Woof!
+                cat.MakeSound(); // Output: Meow!
+            }
+        }
+
+    }
+}
